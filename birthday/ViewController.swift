2f2ff394269daac4f52a03d80b179e467230f63b
gamzeguven <gamze.guven@bil.omu.edu.tr>
2019-01-26 17:08:39 +0300

    Initial Commit

---
+++ birthday/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  birthday
+//
+//  Created by Gamze Güven on 26.01.2019.
+//  Copyright © 2019 Gamze Güven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
